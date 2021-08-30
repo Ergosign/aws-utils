6c05d39af657eb64cfd499d2475a704b5573fdda
Al Briggs <al.briggs@ergosign.de>
2021-08-30 18:14:46 +0200

    support allowed hosts for environments

--- src/cors/option/ValidateCorsHost.ts
+++ src/cors/option/ValidateCorsHost.ts
@@ -1,10 +1,17 @@
 
+export interface AllowedHosts {
+  [index:string] : string[]
+}
+
+
 export class ValidateCorsHost {
 
-  static validateHostName = (originHostname: string): boolean => {
+  
+
+  static validateHostName = (allowedHosts:AllowedHosts, originHostname: string): boolean => {
     const websiteRootDomain = process.env.WEBSITE_ROOT_DOMAIN ?? 'no-root-domain';
     const corsAllowLocalhost = process.env.CORS_ALLOW_LOCALHOST === 'true' ?? false;
-
+    const runningStage = process.env.WEBSITE_ENVIRONMENT_STAGE ?? 'no-stage-set';
     // is the hostname part of the expected root domain?
     if (originHostname.endsWith(websiteRootDomain)){
       return true;
@@ -15,16 +22,22 @@ export class ValidateCorsHost {
       return true;
     }
 
+    // check allowedHosts for the environment
+    if (allowedHosts[runningStage] && allowedHosts[runningStage].find( allowedHost => allowedHost === originHostname) !== undefined){
+      return true;
+    }
+
     return false;
   }
 
-  static generateAllowOriginForOrigin = (inputOrigin:string) => {
+  static generateAllowOriginForOrigin = (allowedHosts:AllowedHosts, inputOrigin:string) => {
     const websiteRootDomain = process.env.WEBSITE_ROOT_DOMAIN || 'no-root-domain';
+    const runningStage = process.env.WEBSITE_ENVIRONMENT_STAGE ?? 'no-stage-set';
     const defaultOrigin = `https://www.${websiteRootDomain}`;
 
     var allowedOrigin : string;
     // Check if request is from one of the allowed servers
-    if (ValidateCorsHost.validateHostName(inputOrigin)){
+    if (ValidateCorsHost.validateHostName(allowedHosts,inputOrigin)){
       allowedOrigin = inputOrigin;
     }else{
       allowedOrigin = defaultOrigin;
--- src/cors/option/__tests__/ValidateCorsHost.spec.ts
+++ src/cors/option/__tests__/ValidateCorsHost.spec.ts
@@ -1,12 +1,12 @@
-import { ValidateCorsHost } from '../ValidateCorsHost';
+import { AllowedHosts } from "./../ValidateCorsHost";
+import { ValidateCorsHost } from "../ValidateCorsHost";
 
-describe('Test if domain valid', () => {
-
+describe("Test if domain valid", () => {
   const OLD_ENV = process.env;
-  const TEST_ROOT_DOMAIN = 'test-root-domain.com';
+  const TEST_ROOT_DOMAIN = "test-root-domain.com";
 
   beforeEach(() => {
-    jest.resetModules() // Most important - it clears the cache
+    jest.resetModules(); // Most important - it clears the cache
     process.env = { ...OLD_ENV }; // Make a copy
   });
 
@@ -14,64 +14,156 @@ describe('Test if domain valid', () => {
     process.env = OLD_ENV; // Restore old environment
   });
 
-  test('Test DEV allows localhost', () => {
+  const emptyAllowedHosts = {};
+
+  test("Test DEV allows localhost", () => {
     // Setup ENV variables for DEV
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'true';
+    process.env.CORS_ALLOW_LOCALHOST = "true";
 
-    expect(ValidateCorsHost.validateHostName('http://localhost:3000')).toBeTruthy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        "http://localhost:3000"
+      )
+    ).toBeTruthy();
   });
 
-  test('Test DEV allows any localhost port ', () => {
+  test("Test DEV allows any localhost port ", () => {
     // Setup ENV variables for DEV
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'true';
+    process.env.CORS_ALLOW_LOCALHOST = "true";
 
-    expect(ValidateCorsHost.validateHostName('http://localhost:8080')).toBeTruthy();
-    expect(ValidateCorsHost.validateHostName('http://localhost:9999')).toBeTruthy();
-    expect(ValidateCorsHost.validateHostName('http://localhost:4321')).toBeTruthy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        "http://localhost:8080"
+      )
+    ).toBeTruthy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        "http://localhost:9999"
+      )
+    ).toBeTruthy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        "http://localhost:4321"
+      )
+    ).toBeTruthy();
   });
 
-  test('Test STAGING does not allow localhost', () => {
+  test("Test STAGING does not allow localhost", () => {
     // Setup ENV variables for STAGING
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'false';
+    process.env.CORS_ALLOW_LOCALHOST = "false";
 
-    expect(ValidateCorsHost.validateHostName('http://localhost:3000')).toBeFalsy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        "http://localhost:3000"
+      )
+    ).toBeFalsy();
   });
 
-  test('Test www in configured root allowed', () => {
+  test("Test www in configured root allowed", () => {
     // Setup ENV variables
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    expect(ValidateCorsHost.validateHostName(`https://www.${TEST_ROOT_DOMAIN}`)).toBeTruthy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        `https://www.${TEST_ROOT_DOMAIN}`
+      )
+    ).toBeTruthy();
   });
 
-  test('Test random other subdomain in configured root allowed', () => {
+  test("Test random other subdomain in configured root allowed", () => {
     // Setup ENV variables
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    expect(ValidateCorsHost.validateHostName(`https://fox-trot.${TEST_ROOT_DOMAIN}`)).toBeTruthy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        `https://fox-trot.${TEST_ROOT_DOMAIN}`
+      )
+    ).toBeTruthy();
   });
 
-
-  test('Test alternate domain not allowed', () => {
+  test("Test alternate domain not allowed", () => {
     // Setup ENV variables
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    expect(ValidateCorsHost.validateHostName(`https://alternate-test-domain.de`)).toBeFalsy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        `https://alternate-test-domain.de`
+      )
+    ).toBeFalsy();
   });
 
-  test('Test alternate subdomain and domain not allowed', () => {
+  test("Test alternate subdomain and domain not allowed", () => {
     // Setup ENV variables
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    expect(ValidateCorsHost.validateHostName(`https://www.alternate-test-domain.de`)).toBeFalsy();
+    expect(
+      ValidateCorsHost.validateHostName(
+        emptyAllowedHosts,
+        `https://www.alternate-test-domain.de`
+      )
+    ).toBeFalsy();
+  });
+
+  test("Test allowedHosts contains", () => {
+    // Setup ENV variables
+    process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
+    process.env.WEBSITE_ENVIRONMENT_STAGE = 'stage';
+    const allowedHosts = {
+      'stage': ['https://allowed-host-approved.de']
+    }
+    expect(
+      ValidateCorsHost.validateHostName(
+        allowedHosts,
+        `https://allowed-host-approved.de`
+      )
+    ).toBeTruthy();
+  });
+
+  test("Test allowedHosts doesn't contains", () => {
+    // Setup ENV variables
+    process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
+    process.env.WEBSITE_ENVIRONMENT_STAGE = 'stage';
+    const allowedHosts = {
+      'stage': ['https://allowed-host-approved.de']
+    }
+    expect(
+      ValidateCorsHost.validateHostName(
+        allowedHosts,
+        `https://not-allowed-host-approved.de`
+      )
+    ).toBeFalsy();
+  });
+
+  test("Test allowedHosts doesn't contains for stage", () => {
+    // Setup ENV variables
+    process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
+    process.env.WEBSITE_ENVIRONMENT_STAGE = 'production';
+    const allowedHosts = {
+      'stage': ['https://allowed-host-approved.de']
+    }
+    expect(
+      ValidateCorsHost.validateHostName(
+        allowedHosts,
+        `https://allowed-host-approved.de`
+      )
+    ).toBeFalsy();
   });
 });
 
-describe('Test allowed origin for domains', () => {
+describe("Test allowed origin for domains", () => {
   const OLD_ENV = process.env;
-  const TEST_ROOT_DOMAIN = 'test-root-domain.com';
+  const TEST_ROOT_DOMAIN = "test-root-domain.com";
+  const emptyAllowedHosts = {};
 
   beforeEach(() => {
-    jest.resetModules() // Most important - it clears the cache
+    jest.resetModules(); // Most important - it clears the cache
     process.env = { ...OLD_ENV }; // Make a copy
   });
 
@@ -79,45 +171,85 @@ describe('Test allowed origin for domains', () => {
     process.env = OLD_ENV; // Restore old environment
   });
 
-
-  test('DEV + localhost', () => {
+  test("DEV + localhost", () => {
     // Setup ENV variables for DEV
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'true';
+    process.env.CORS_ALLOW_LOCALHOST = "true";
 
-    expect(ValidateCorsHost.generateAllowOriginForOrigin('http://localhost:3000')).toMatch('http://localhost:3000');
+    expect(
+      ValidateCorsHost.generateAllowOriginForOrigin(emptyAllowedHosts,"http://localhost:3000")
+    ).toMatch("http://localhost:3000");
   });
 
-  test('STAGING + localhost', () => {
+  test("STAGING + localhost", () => {
     // Setup ENV variables for DEV
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'false';
+    process.env.CORS_ALLOW_LOCALHOST = "false";
 
-    expect(ValidateCorsHost.generateAllowOriginForOrigin('http://localhost:3000')).toMatch(`https://www.${TEST_ROOT_DOMAIN}`);
+    expect(
+      ValidateCorsHost.generateAllowOriginForOrigin(emptyAllowedHosts,"http://localhost:3000")
+    ).toMatch(`https://www.${TEST_ROOT_DOMAIN}`);
   });
 
-  test('random request', () => {
+  test("random request", () => {
     // Setup ENV variables for DEV
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'false';
+    process.env.CORS_ALLOW_LOCALHOST = "false";
 
-    expect(ValidateCorsHost.generateAllowOriginForOrigin('http://abc-1231.de')).toMatch(`https://www.${TEST_ROOT_DOMAIN}`);
+    expect(
+      ValidateCorsHost.generateAllowOriginForOrigin(emptyAllowedHosts,"http://abc-1231.de")
+    ).toMatch(`https://www.${TEST_ROOT_DOMAIN}`);
   });
 
-  test('random request DEV', () => {
+  test("random request DEV", () => {
     // Setup ENV variables for DEV
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'true';
+    process.env.CORS_ALLOW_LOCALHOST = "true";
 
-    expect(ValidateCorsHost.generateAllowOriginForOrigin('http://abc-1231.de')).toMatch(`https://www.${TEST_ROOT_DOMAIN}`);
+    expect(
+      ValidateCorsHost.generateAllowOriginForOrigin(emptyAllowedHosts,"http://abc-1231.de")
+    ).toMatch(`https://www.${TEST_ROOT_DOMAIN}`);
   });
 
-  test('random subdomain', () => {
+  test("random subdomain", () => {
     // Setup ENV variables for DEV
     process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
-    process.env.CORS_ALLOW_LOCALHOST = 'true';
+    process.env.CORS_ALLOW_LOCALHOST = "true";
 
-    expect(ValidateCorsHost.generateAllowOriginForOrigin(`https://random-sub-domain.${TEST_ROOT_DOMAIN}`)).toMatch(`https://random-sub-domain.${TEST_ROOT_DOMAIN}`);
+    expect(
+      ValidateCorsHost.generateAllowOriginForOrigin(emptyAllowedHosts,
+        `https://random-sub-domain.${TEST_ROOT_DOMAIN}`
+      )
+    ).toMatch(`https://random-sub-domain.${TEST_ROOT_DOMAIN}`);
   });
 
+  test("allowedHost", () => {
+    // Setup ENV variables for DEV
+    process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
+    process.env.CORS_ALLOW_LOCALHOST = "true";
+    process.env.WEBSITE_ENVIRONMENT_STAGE = 'stage';
+    const allowedHosts = {
+      'stage': ['https://allowed-host-approved.de']
+    }
+    expect(
+      ValidateCorsHost.generateAllowOriginForOrigin(allowedHosts,
+        `https://allowed-host-approved.de`
+      )
+    ).toMatch(`https://allowed-host-approved.de`);
+  });
+
+  test("not allowedHost", () => {
+    // Setup ENV variables for DEV
+    process.env.WEBSITE_ROOT_DOMAIN = TEST_ROOT_DOMAIN;
+    process.env.CORS_ALLOW_LOCALHOST = "true";
+    process.env.WEBSITE_ENVIRONMENT_STAGE = 'stage';
+    const allowedHosts = {
+      'stage': ['https://allowed-host-approved.de']
+    }
+    expect(
+      ValidateCorsHost.generateAllowOriginForOrigin(allowedHosts,
+        `https://not-allowed-host-approved.de`
+      )
+    ).toMatch(`https://www.${TEST_ROOT_DOMAIN}`);
+  });
 });
